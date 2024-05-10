4e343946949cce25dc71171686ca759f5df37717
Eshita Bhattacharyya <eshita.bhattacharyya@pwc.com>
2024-05-10 12:13:32 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,28 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "East US"
+
+
+}
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "East US"
+  resource_group_name = "myTFResourceGroup"
+}
